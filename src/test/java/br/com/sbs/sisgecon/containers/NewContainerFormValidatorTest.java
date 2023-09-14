ca82660cefd7305e3ed4c9ac5b3e5cd484af0284
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-09-14 12:16:48 -0300

    tests validations

---
+++ src/test/java/br/com/sbs/sisgecon/containers/NewContainerFormValidatorTest.java
@@ -0,0 +1,42 @@
+package br.com.sbs.sisgecon.containers;
+
+import br.com.sbs.sisgecon.containers.dto.NewContainerForm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.Errors;
+
+import static org.mockito.Mockito.*;
+
+class NewContainerFormValidatorTest {
+
+    private ContainerRepository containerRepository;
+    private NewContainerFormValidator newContainerFormValidator;
+    private Errors errors;
+
+    @BeforeEach
+    void setUp() {
+        containerRepository = mock(ContainerRepository.class);
+        newContainerFormValidator = new NewContainerFormValidator(containerRepository);
+        errors = mock(Errors.class);
+    }
+
+    @Test
+    void validate__should_not_return_error_when_container_does_not_exists() {
+        NewContainerForm newContainerForm = mock(NewContainerForm.class);
+        when(containerRepository.existsByNumber(newContainerForm.number())).thenReturn(false);
+
+        newContainerFormValidator.validate(newContainerForm, errors);
+
+        verify(errors, never()).rejectValue("number", "container.already.exists");
+    }
+
+    @Test
+    void validate__should_return_error_when_container_already_exists() {
+        NewContainerForm newContainerForm = mock(NewContainerForm.class);
+        when(containerRepository.existsByNumber(newContainerForm.number())).thenReturn(true);
+
+        newContainerFormValidator.validate(newContainerForm, errors);
+
+        verify(errors).rejectValue("number", "container.already.exists");
+    }
+}
