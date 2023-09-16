de4d2041d56efc51d2d2b4f3264fd2ae386d7706
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-09-16 16:05:15 -0300

    rename application class

--- src/main/java/br/com/sbs/sisgeconapi/SisgeconAPIApplication.java
+++ src/main/java/br/com/sbs/sisgeconapi/SisgeconAPIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SisgeconApplication {
+public class SisgeconAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SisgeconApplication.class, args);
+		SpringApplication.run(SisgeconAPIApplication.class, args);
 	}
 
 }
